ff0191873ae0939de9367890fdd33f0bf68e2493
tomekl007 <tomekl007@gmail.com>
2015-01-22 15:00:26 +0100

    zad7 finished

---
+++ src/main/java/zad7/DisplaySystem.java
@@ -0,0 +1,124 @@
+package zad7;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Tomasz Lelek
+ * @since 2015-01-22
+ */
+public class DisplaySystem implements DisplaySystemInterface {
+  Map<Integer, Display> displays = new LinkedHashMap<Integer, Display>();
+  Map<Integer, List<Integer>> groupOfDisplays = new LinkedHashMap<Integer, List<Integer>>();
+  private static final AtomicInteger displaysCounter = new AtomicInteger();
+  private static final AtomicInteger groupCounter = new AtomicInteger();
+
+  @Override
+  public int registerDisplay(int rows) {
+    int id = displaysCounter.getAndIncrement();
+    displays.put(id, new Display(rows, id));
+    return id;
+  }
+
+  @Override
+  public int createGroup() {
+    int id = groupCounter.incrementAndGet();
+    groupOfDisplays.put(id, new LinkedList<Integer>());
+    return id;
+  }
+
+  @Override
+  public boolean addDisplayToGroup(int displayID, int groupID) {
+    if(validateGroupOfDisplays(groupID)){
+      return false;
+    }
+    if(validateDisplays(displayID)){
+      return false;
+    }
+
+    List<Integer> displaysForThatGroup = groupOfDisplays.get(groupID);
+    if(displaysForThatGroup == null ){
+      displaysForThatGroup = new LinkedList<Integer>();
+    }
+    displaysForThatGroup.add(displayID);
+    return true;
+
+  }
+
+  private boolean validateDisplays(int displayID) {
+    return !displays.containsKey(displayID);
+  }
+
+  @Override
+  public boolean removeGroup(int groupID) {
+    if(validateGroupOfDisplays(groupID)){
+      return false;
+    }
+    groupOfDisplays.remove(groupID);
+    return true;
+  }
+
+  private boolean validateGroupOfDisplays(int groupID) {
+    return !groupOfDisplays.containsKey(groupID);
+  }
+
+  @Override
+  public boolean deregisterDisplay(int displayID) {
+    if(validateDisplays(displayID)){
+      return false;
+    }
+    displays.remove(displayID);
+    return true;
+  }
+
+  @Override
+  public boolean toDisplay(int displayID, String message) {
+    if(validateDisplays(displayID)){
+      return false;
+    }
+    displays.get(displayID).addMessage(message);
+    return true;
+  }
+
+  @Override
+  public boolean toGroup(int groupID, String message) {
+    if(validateGroupOfDisplays(groupID)){
+      return false;
+    }
+    for(Integer displayId : groupOfDisplays.get(groupID)){
+      displays.get(displayId).addMessage(message);
+    }
+    return true;
+  }
+
+  @Override
+  public String[] get(int displayID) {
+    if(validateDisplays(displayID)){
+      return null;
+    }
+    List<String> messagesToDisplay = displays.get(displayID).messagesToDisplay;
+    int size = messagesToDisplay.size();
+
+    if(size > displays.get(displayID).rows){
+      List<String> subList = messagesToDisplay.subList(size - displays.get(displayID).rows, size);
+      return subList.toArray(new String[subList.size()]);
+    }
+    return messagesToDisplay.toArray(new String[messagesToDisplay.size()]);
+  }
+
+  class Display{
+    int id;
+    int rows;
+    List<String> messagesToDisplay;
+
+    public Display(int rows, int id) {
+      this.rows = rows;
+      this.id = id;
+      this.messagesToDisplay = new ArrayList<String>();
+    }
+
+    public void addMessage(String message){
+      messagesToDisplay.add(message);
+    }
+  }
+}
---
+++ src/main/java/zad7/DisplaySystemInterface.java
@@ -0,0 +1,72 @@
+package zad7;
+
+/**
+ * Interfejs obslugi systemu wyswietlaczy alfanumerycznych.
+ * @author oramus
+ *
+ */
+public interface DisplaySystemInterface {
+	/**
+	 * Rejestruje wyswietlacz w systemie. Wyswietlacz uzyskuje unikalny identyfikator.
+	 * @param rows liczba wierszy tekstu
+	 * @return unikalny identyfikator wyswietlacza w systemie.
+	 */
+	public int registerDisplay( int rows );
+	
+	/**
+	 * Tworzy grupe wyswietlaczy. Grupie nadawany jest unikalny identyfikator.
+	 * @return unikalny identyfikator grupy
+	 */
+	public int createGroup();
+	
+	/**
+	 * Dodaje wyswietlacz do grupy.
+	 * @param displayID identyfikator wyswietlacza
+	 * @param groupID identyfikator grupy, do ktorej wyswietlacza jest dodawany
+	 * @return true - operacja zakonczona sukcesem, false - operacja nie mogla zostac wykonana ze wzgledu na
+	 * dostarczenie blednych danych
+	 */
+	public boolean addDisplayToGroup( int displayID, int groupID );
+	
+	/**
+	 * Usuwa grupe z systemu.
+	 * @param groupID identyfikator usuwanej grupy
+	 * @return true - operacja zakonczona sukcesem, false - blad na skutek blednych danych wejsciowych
+	 */
+	public boolean removeGroup( int groupID );
+	
+	/**
+	 * Odlacza wyswietlacz od systemu
+	 * @param displayID identyfikator wyswietlacza odlaczanego od systemu
+	 * @return true - operacja zakonczona sukcesem, false - blad na skutek blednych danych wejsciowych
+	 */
+	public boolean deregisterDisplay( int displayID );
+
+	/**
+	 * Przesyla jedna linike tekstu do wybranego wyswietlacza.
+	 * @param displayID identyfikator wyswietlacza 
+	 * @param message przekazana wiadomosc
+	 * @return true - operacja zakonczona sukcesem, false - blad na skutek blednych danych wejsciowych
+	 */
+	public boolean toDisplay( int displayID, String message );
+
+	/**
+	 * Przesyla jedna linijke tekstu do wszystkich wyswietlaczy w grupie 
+	 * @param groupID identyfikator grupy
+	 * @param message przekazana wiadomosc
+	 * @return true - operacja zakonczona sukcesem, false - blad na skutek blednych danych wejsciowych
+	 */
+	public boolean toGroup( int groupID, String message );
+	
+	/**
+	 * Zwraca wiadomosci przeslane do wyswietlacza o danym identyfikatorze. Tablica 
+	 * nie moze byc dluzsza od ilosci wierszy wyswietlacza. Wiadomosci
+	 * ulozone sa w tablicy wynikowej w kolejnosci ich naplywania: od najstarszych (u gory)
+	 * do najnowszych (na dole).
+	 * @param displayID - identyfikator wyswietlacza
+	 * @return - tablica zawierajace wiadomosci przeslane do wswietlacza z uwzglednieniem
+	 * ilosci wierszy tekstu, jakie moze on wyswietlac. null - dla blednego identyfikatora lub
+	 * braku wiadomosci.
+	 */
+	public String[] get( int displayID );
+}
