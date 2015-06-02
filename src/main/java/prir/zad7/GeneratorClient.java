fdbf1ad81dbaf05d64d92ee0b229c1ffab84eddf
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-06-02 20:43:58 +0200

    zad7 impl

--- src/main/java/prir/zad7/GeneratorClient.java
+++ src/main/java/prir/zad7/GeneratorClient.java
@@ -16,9 +16,23 @@ public class GeneratorClient {
   org.omg.CORBA.Object envObj = namingContext.resolve( path );
   optimization c = optimizationHelper.narrow( envObj );
   IntHolder ih = new IntHolder();
+     IntHolder ih2 = new IntHolder();
+     IntHolder ih3 = new IntHolder();
   
-  c.register((short)1, 100, ih);
+     c.register((short)1, 10000, ih);
+     c.register((short)2, 5000, ih2);
+     c.register((short)3, 4000, ih3);
      System.out.println("get id : "+ ih.value);
+     Thread.sleep(1000);
+     c.hello(ih3.value);
+     Thread.sleep(5000);
+     rangeHolder rh = new rangeHolder();
+     c.best_range(rh);
+     System.out.println("rh.from " +  rh.value.from);
+     System.out.println("rh.to " +  rh.value.to);
 
  }
+  
+    
+    
 }
--- src/main/java/prir/zad7/optimizationImpl.java
+++ src/main/java/prir/zad7/optimizationImpl.java
@@ -1,9 +1,10 @@
 //package prir.zad7;
 
 import org.omg.CORBA.IntHolder;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,36 +12,108 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class optimizationImpl extends optimizationPOA {
     AtomicInteger clientId = new AtomicInteger();
-    Map<Integer, ClientData> clientDataMap = new LinkedHashMap<>();
+    Map<Short, ClientData> clientDataMap = new TreeMap<>();
+    public optimizationImpl(){
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new CountAvailability(clientDataMap), 0, 1, TimeUnit.MILLISECONDS);
+    }
     
     @Override
     public void register(short ip, int timeout, IntHolder id) {
         System.out.println("register srever : " + ip + " with timeout : " + timeout);
         int idG = clientId.incrementAndGet();
         id.value = idG;
-        clientDataMap.put(idG, new ClientData(ip, timeout));
+        clientDataMap.put(ip, new ClientData(idG, timeout));
     }
 
     @Override
     public void hello(int id) {
+        System.out.println("hello for id : "+ id);
+        Set<Map.Entry<Short, ClientData>> entries = clientDataMap.entrySet();
+        for(Map.Entry<Short, ClientData> entry : entries){
+            if(entry.getValue().id == id){
+                entry.getValue().refreshCounter();
+            }
+        }
 
     }
 
     @Override
-    public void best_range(rangeHolder r) {
-
+    public void best_range(rangeHolder r) {//tod test it properly
+        short startRange=-1;
+        short endRange=255;
+        int lengthOfRange;
+        Set<Map.Entry<Short, ClientData>> entries = clientDataMap.entrySet();
+        
+        short index = 0;
+        for(Map.Entry<Short, ClientData> entry : entries){
+            if(entry.getValue().working && startRange != -1){
+                endRange = index;
+            }else if(entry.getValue().working){
+                startRange = index;
+                endRange = index;
+            }else if(! entry.getValue().working) {
+                startRange = index;
+                endRange = index;
+            }
+            
+            index++;
+        }
+        r.value = new range(startRange, endRange);
     }
 }
 
 class ClientData{
+    @Override
+    public String toString() {
+        return "ClientData{" +
+                "id=" + id +
+                ", timeout=" + timeout +
+                ", working=" + working +
+                ", counter=" + counter +
+                '}';
+    }
 
-
-    private final short ip;
+//    private final short ip;
     private final int timeout;
+    boolean working;
+    AtomicInteger counter;
+    public final int id;
 
-    public ClientData(short ip, int timeout) {
+    public ClientData(int id, int timeout) {
 
-        this.ip = ip;
+//        this.ip = ip;
         this.timeout = timeout;
+        this.working = true;
+        this.counter = new AtomicInteger(timeout);
+        this.id = id;
+    }
+
+    public void refreshCounter() {
+        counter = new AtomicInteger(timeout);
     }
 }
+
+class CountAvailability implements Runnable{
+
+    private Map<Short, ClientData> clientDataMap;
+
+    public CountAvailability(Map<Short, ClientData> clientDataMap) {
+
+        this.clientDataMap = clientDataMap;
+    }
+
+    @Override
+    public void run() {
+        Set<Map.Entry<Short, ClientData>> entries = clientDataMap.entrySet();
+        for(Map.Entry<Short, ClientData> entry : entries){
+            if(entry.getValue().counter.get() == 0){
+                entry.getValue().working = false;
+            }else {
+                entry.getValue().counter.decrementAndGet();
+                System.out.println("decrement for : "+ entry);
+            }
+        }
+    }
+}
+
