799ceeccd69d4484d16bf82dbab6e95ff8365d92
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-06-10 11:13:55 +0200

    new best_range impl

--- src/test/java/prir/zad7/RangeTest.java
+++ src/test/java/prir/zad7/RangeTest.java
@@ -30,12 +30,54 @@ public class RangeTest {
         range range = new RangeTest().best_range(clientDataMap);
         
         //then
-        assertThat(range.bestStartRange).isEqualTo(((short)4));
-        assertThat(range.bestEndRange).isEqualTo((short)6);
+        assertThat(range.bestStartIp).isEqualTo(((short)4));
+        assertThat(range.bestEndIp).isEqualTo((short)6);
         
 
     }
 
+
+    @Test//todo make that test pass
+    public void shouldFindRangeIndexAgnostic(){
+        //given
+        Map<Short, ClientData> clientDataMap = new TreeMap<>();
+        clientDataMap.put((short)11, new ClientData(true));
+        clientDataMap.put((short)12, new ClientData(true));
+        clientDataMap.put((short)13, new ClientData(false));
+        clientDataMap.put((short)14, new ClientData(true));
+        clientDataMap.put((short)15, new ClientData(true));
+        clientDataMap.put((short)16, new ClientData(true));
+
+        //when
+        range range = new RangeTest().best_range(clientDataMap);
+
+        //then
+        assertThat(range.bestStartIp).isEqualTo(((short)14));
+        assertThat(range.bestEndIp).isEqualTo((short)16);
+
+
+    }
+
+
+    @Test
+    public void shouldFindRangeNotLinear(){
+        //given
+        Map<Short, ClientData> clientDataMap = new TreeMap<>();
+        clientDataMap.put((short)1, new ClientData(true));
+        clientDataMap.put((short)3, new ClientData(true));
+        clientDataMap.put((short)4, new ClientData(true));
+        clientDataMap.put((short)5, new ClientData(true));
+
+        //when
+        range range = new RangeTest().best_range(clientDataMap);
+        System.out.println("---> " + range);
+
+        //then
+        assertThat(range.bestStartIp).isEqualTo(((short)3));
+        assertThat(range.bestEndIp).isEqualTo((short)5);
+
+
+    }
 
 
     @Test
@@ -53,8 +95,8 @@ public class RangeTest {
         range range = new RangeTest().best_range(clientDataMap);
 
         //then
-        assertThat(range.bestStartRange).isEqualTo(((short)1));
-        assertThat(range.bestEndRange).isEqualTo((short)6);
+        assertThat(range.bestStartIp).isEqualTo(((short)1));
+        assertThat(range.bestEndIp).isEqualTo((short)6);
 
 
     }
@@ -75,8 +117,8 @@ public class RangeTest {
         range range = new RangeTest().best_range(clientDataMap);
 
         //then
-        assertThat(range.bestStartRange).isEqualTo(((short)1));
-        assertThat(range.bestEndRange).isEqualTo((short)3);
+        assertThat(range.bestStartIp).isEqualTo(((short)1));
+        assertThat(range.bestEndIp).isEqualTo((short)3);
 
 
     }
@@ -99,19 +141,26 @@ public class RangeTest {
         range range = new RangeTest().best_range(clientDataMap);
 
         //then
-        assertThat(range.bestStartRange).isEqualTo(((short)0));
-        assertThat(range.bestEndRange).isEqualTo((short)0);
+        assertThat(range.bestStartIp).isEqualTo(((short)0));
+        assertThat(range.bestEndIp).isEqualTo((short)0);
 
 
     }
-    
-    
+
+
     public range best_range(Map<Short, ClientData> clientDataMap) {//tod test it properly
+        //todo 1 3 4 5 -> 3 - 5
+        System.out.println("bestRange for : "+ clientDataMap);
         short startRange=0;
+        short startIp=0;
         short endRange=0;
+        short endIp = 0;
         int lengthOfRange = -1;
 
+
+        short bestStartIp = 0;
         short bestStartRange = 0;
+        short bestEndIp = 0;
         short bestEndRange = 0;
         int bestLengthOfRange = -1;
         Set<Map.Entry<Short, ClientData>> entries = clientDataMap.entrySet();
@@ -123,13 +172,20 @@ public class RangeTest {
                     bestLengthOfRange = lengthOfRange;
                     bestStartRange = startRange;
                     bestEndRange = endRange;
+                    bestStartIp = startIp;
+                    bestEndIp = endIp;
                 }
                 startRange = 0;
                 endRange = 0;
+                startIp = 0;
+                endIp = 0;
                 lengthOfRange = -1;
             }else if(entry.getValue().working && startRange != 0){
+                endIp = entry.getKey();
                 endRange = index;
             }else if(entry.getValue().working){
+                startIp = entry.getKey();
+                endIp = entry.getKey();
                 startRange = index;
                 endRange = index;
             }
@@ -140,12 +196,15 @@ public class RangeTest {
         if(lengthOfRange > bestLengthOfRange){
             bestStartRange = startRange;
             bestEndRange = endRange;
+            bestStartIp = startIp;
+            bestEndIp = endIp;
         }
-        
-        
-        
-        return new range(bestStartRange, bestEndRange);
+
+        range range = new range(bestStartRange, bestEndRange, bestStartIp, bestEndIp);
+        System.out.println("--> returns : "+ range);
+        return range;
     }
+
 
 
 
@@ -185,18 +244,33 @@ class ClientData{
         counter = new AtomicInteger(timeout);
     }
 }
-    
+
 class range{
 
 
     public final short bestStartRange;
     public final short bestEndRange;
+    public final short bestStartIp;
+    public final short bestEndIp;
 
-    public range(short bestStartRange, short bestEndRange) {
+    @Override
+    public String toString() {
+        return "RangeClass{" +
+                "bestStartRange=" + bestStartRange +
+                ", bestEndRange=" + bestEndRange +
+                ", bestStartIp=" + bestStartIp +
+                ", bestEndIp=" + bestEndIp +
+                '}';
+    }
+
+    public range(short bestStartRange, short bestEndRange, short bestStartIp, short bestEndIp) {
 
         this.bestStartRange = bestStartRange;
         this.bestEndRange = bestEndRange;
+        this.bestStartIp = bestStartIp;
+        this.bestEndIp = bestEndIp;
     }
 }
+
 
     
